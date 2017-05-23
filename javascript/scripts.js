96bae3175135276c6632534d5cd637a926219e3e
Christine Callo and Lia Carpeneti <lia@>
2017-05-22 20:42:13 -0700

    added night and day view

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
+  $(".text-white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+    $(".text-black").show();
+    $(".text-white").hide();
+  });
+  
+  $(".text-black").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+    $(".text-white").show();
+    $(".text-black").hide();
+  });
+
   $(".peekaboo").click(function() {
     $(".walrus-hidden").toggle();
     $(".walrus-showing").toggle();
